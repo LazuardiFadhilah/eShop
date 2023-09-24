dc75799d78e5a7fd87d0074aad007d3e5fd3d36e
Lazuardi Fadhilah <Lazuardifadhillah@gmail.com>
2023-09-24 13:38:17 +0700

    sign up page update

--- lib/screens/signup.dart
+++ lib/screens/signup.dart
@@ -14,9 +14,28 @@ class SignUp extends StatelessWidget {
         top: true,
         child: Padding(
           padding: EdgeInsets.all(20),
-          child: CustomTextField(
-            title: 'Full name',
-            desc: 'Enter your full name',
+          child: Column(
+            children: [
+              CustomTextField(
+                title: 'Full name',
+                desc: 'Enter your full name',
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              CustomTextField(
+                title: 'Email',
+                desc: 'Enter your email address',
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              CustomTextField(
+                title: 'Password',
+                desc: 'Enter your password',
+                isObscured: true,
+              ),
+            ],
           ),
         ),
       ),
